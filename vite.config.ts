a5a4608c749da7fd57339b0121ea81eb4660f8cf
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-04 20:42:18 +0800

    :wrench: 配置路径别名 `@`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import Vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
 	plugins: [
 		UnoCSS(),
 		Vue()
