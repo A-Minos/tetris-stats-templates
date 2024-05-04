8dfaa5415f26d311c016931f770b7bdba4eb848c
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-04 22:31:10 +0800

    :bug: 不支持 `top await`, 用 `iife` 包裹一下

--- src/main.ts
+++ src/main.ts
@@ -7,45 +7,47 @@ import { data } from '@/core/template'
 import { createApp, h } from 'vue'
 import { createMemoryHistory, createRouter, RouterView } from 'vue-router'
 
-try {
-	const element = document.createElement('div')
-	const app = createApp({
-		render: () => h(RouterView)
-	})
-
-	const router = createRouter({
-		history: createMemoryHistory(),
-		routes: [
-			{
-				path: '/',
-				component: Home
-			},
-			{
-				path: '/binding',
-				component: Binding
-			}
-		]
-	})
-
-	app.use(router)
-	app.mount(element)
-
-	const targetPath = `/${data.value.type}`
-
-	const routes = router.getRoutes()
-	const routeExists = routes.some(route => {
-		return targetPath === route.path
-	})
-
-	if (routeExists) {
-		await router.push({
-			path: targetPath
+(async () => {
+	try {
+		const element = document.createElement('div')
+		const app = createApp({
+			render: () => h(RouterView)
 		})
-	}
 
-	document.body.appendChild(element)
-} catch (e) {
-	if (import.meta.env.DEV) {
-		console.error(e)
+		const router = createRouter({
+			history: createMemoryHistory(),
+			routes: [
+				{
+					path: '/',
+					component: Home
+				},
+				{
+					path: '/binding',
+					component: Binding
+				}
+			]
+		})
+
+		app.use(router)
+		app.mount(element)
+
+		const targetPath = `/${data.value.type}`
+
+		const routes = router.getRoutes()
+		const routeExists = routes.some(route => {
+			return targetPath === route.path
+		})
+
+		if (routeExists) {
+			await router.push({
+				path: targetPath
+			})
+		}
+
+		document.body.appendChild(element)
+	} catch (e) {
+		if (import.meta.env.DEV) {
+			console.error(e)
+		}
 	}
-}
+})()
