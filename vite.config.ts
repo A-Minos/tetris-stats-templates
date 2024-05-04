673baec47f57ec1b987372b21dc58f4a0974c3f8
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-04 19:28:16 +0800

    :wrench: 添加 `unocss` 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import Vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config
 export default defineConfig({
 	plugins: [
+		UnoCSS(),
 		Vue()
 	]
 })
