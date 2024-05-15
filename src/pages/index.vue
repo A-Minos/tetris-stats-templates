a839baa6004f15214a0c9f5ad95ace0cd6b75ddf
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-05-15 16:47:59 +0800

    :lipstick: 更新首页

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,11 +17,46 @@ const tests = asyncComputed(async () => {
 </script>
 
 <template>
-	<div class="flex flex-col gap-1 p-2">
-		<span>按住 ctrl 以修改值</span>
+	<div class="index">
+		<div class="index__container">
+			<div class="index__title">俄罗斯方块 Bot 模板</div>
+			<div class="index__tips">(按住 ctrl 以修改值)</div>
 
-		<div class="flex gap-1">
-			<Component :is="test" v-for="test in tests"/>
+			<div class="index__divider"/>
+
+			<div class="index__buttons">
+				<Component :is="test" v-for="test in tests"/>
+			</div>
 		</div>
 	</div>
 </template>
+
+<style lang="scss" scoped>
+.index {
+	@apply w-screen h-screen bg-black flex flex-col justify-center items-center;
+
+	&__container {
+		@apply flex flex-col gap-1 p-2 font-sans;
+	}
+
+	&__title {
+		@apply text-8 text-center text-white;
+	}
+
+	&__tips {
+		@apply text-5 text-center text-white;
+	}
+
+	&__divider {
+		@apply w-full border-b-2 border-b-solid border-gray;
+	}
+
+	&__buttons {
+		@apply flex flex-col items-start gap-1;
+
+		& > :deep(button) {
+			@apply text-6 transition cursor-pointer text-white hover:opacity-50 bg-transparent border-none;
+		}
+	}
+}
+</style>
