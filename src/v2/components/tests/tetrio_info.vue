2c4dcb2693f7ac9493fac521fbb43c71e71376ed
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-06-10 02:42:18 +0800

    :sparkles: 更新测试数据, 支持自调试

--- src/v2/components/tests/tetrio_info.vue
+++ src/v2/components/tests/tetrio_info.vue
@@ -1,85 +1,190 @@
 <script lang="ts" setup>
+import { md5 } from 'hash-wasm'
+import { isNonNullish } from 'remeda'
+
 const path = 'v2/tetrio/info'
 
-const test = () => {
+const test = async () => {
+	const user = await fetch('/_proxy/tetrio/users/5eb3a6530b29196c155074e8')
+		.then(response => {
+			return response.json()
+		})
+		.then(result => {
+			return (result as {
+				readonly data: {
+					readonly user: {
+						readonly _id: string
+						readonly username: string
+
+						readonly badges: {
+							readonly id: string
+							readonly label: string
+							readonly group: string | null
+							readonly ts: string | null
+						}[]
+
+						readonly country: string
+						readonly supporter_tier: number
+						readonly xp: number
+						readonly avatar_revision: number
+						readonly gametime: number
+						readonly friend_count: number
+						readonly verified: boolean
+						readonly badstanding?: boolean
+						readonly gamesplayed: number
+						readonly gameswon: number
+						readonly bio: string
+						readonly ts: string
+
+						readonly league: {
+							readonly rank: string
+							readonly bestrank: string
+							readonly rating: number
+							readonly glicko: number
+							readonly rd: number
+							readonly pps: number
+							readonly apm: number
+							readonly vs: number
+							readonly gamesplayed: number
+							readonly gameswon: number
+							readonly decaying: number
+						}
+
+						readonly banner_revision: number
+					}
+				}
+			}).data.user
+		})
+
+	const data = await fetch(`/_proxy/tetrio/users/${user._id}/records`)
+		.then(response => {
+			return response.json()
+		})
+		.then(result => {
+			return (result as {
+				readonly data: {
+					readonly records: {
+						readonly '40l': {
+							readonly record: {
+								readonly endcontext: {
+									readonly finalTime: number
+								}
+
+								readonly ts: string
+							} | null
+
+							readonly rank: number | null
+						}
+
+						readonly blitz: {
+							readonly record: {
+								readonly endcontext: {
+									readonly score: number
+								}
+
+								readonly ts: string
+							} | null
+
+							readonly rank: number | null
+						}
+					}
+
+					readonly zen: {
+						readonly level: number
+						readonly score: number
+					}
+				}
+			}).data
+		})
+
 	document.querySelector('template#data')!.innerHTML = JSON.stringify({
 		user: {
-			id: '5eb3a6530b29196c155074e8',
-			name: 'WOSHIZHAZHA120',
-			avatar: 'https://tetr.io/user-content/avatars/5eb3a6530b29196c155074e8.jpg',
-			banner: 'https://tetr.io/user-content/banners/605468e1d8bf2a23b076eba5.jpg',
-			badges: [
-				{
-					id: 'secretgrade',
-					label: 'Achieved the full Secret Grade',
-					group: null,
-					ts: null
-				},
-				{
-					id: '100player',
-					label: 'Emerged victorious in a 100+ player game',
-					group: null,
-					ts: '2023-06-02T13:20:29.225Z'
-				},
-				{
-					id: '20tsd',
-					label: 'Cleared 40 LINES with only T-Spin Doubles',
-					group: null,
-					ts: '2023-08-19T09:00:24.017Z'
+			id: user._id,
+			name: user.username,
+			avatar: isNonNullish(user.avatar_revision) ? `https://tetr.io/user-content/avatars/${user._id}.jpg?rv=${user.avatar_revision}` : {
+				type: 'identicon',
+				hash: await md5(user._id)
+			},
+			banner: user.banner_revision > 0 ? `https://tetr.io/user-content/banners/${user._id}.jpg?rv=${user.banner_revision}` : null,
+			badges: user.badges.map(badge => {
+				return {
+					id: badge.id,
+					description: badge.label,
+					group: badge.group,
+					receive_at: badge.ts
 				}
-			],
-			country: 'cn',
-			xp: 15033024,
-			friend_count: 184,
-			playtime: '1079 小时',
-			supporter_tier: 3,
-			bad_standing: true,
-			verified: true,
-			bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			join_at: '2020/5/7 14:10:27'
+			}),
+			country: user.country,
+			xp: user.xp,
+			friend_count: user.friend_count,
+			playtime: `${Math.trunc(user.gametime / 3600)} 小时`,
+			supporter_tier: user.supporter_tier,
+			bad_standing: user.badstanding ?? false,
+			verified: user.verified,
+			bio: user.bio,
+			join_at: user.ts
 		},
-		tetra_league: {
-			rank: 'x',
-			highest_rank: 'x',
+		tetra_league: user.league.rating !== -1 ? {
+			rank: user.league.rank,
+			highest_rank: user.league.bestrank,
 
-			tr: 24830.47,
-			glicko: 2806.04,
-			rd: 62.27,
+			tr: Number(
+				user.league.rating.toFixed(2)
+			),
+			glicko: Number(
+				user.league.glicko.toFixed(2)
+			),
+			rd: Number(
+				user.league.rd.toFixed(2)
+			),
 			global_rank: 327,
 			country_rank: 16,
 
-			pps: 2.97,
+			pps: user.league.pps,
 
-			apm: 119.32,
-			apl: 1.67,
+			apm: user.league.apm,
+			apl: Number(
+				(user.league.apm / user.league.pps / 24).toFixed(2)
+			),
 
-			vs: 234.00,
-			adpl: 1.96,
+			vs: user.league.vs,
+			adpl: Number(
+				(user.league.vs / user.league.pps / 24 * 0.6).toFixed(2)
+			),
 
 			statistic: {
-				total: 5700,
-				wins: 3004
+				total: user.league.gamesplayed,
+				wins: user.league.gameswon
 			},
 
-			decaying: false
-		},
+			decaying: user.league.decaying
+		} : null,
 		statistic: {
-			total: 7625,
-			wins: 3747
+			total: user.gamesplayed === -1 ? null : user.gamesplayed,
+			wins: user.gameswon === -1 ? null : user.gameswon
 		},
-		sprint: {
-			time: '0:22.293',
-			global_rank: 367,
-			play_at: '2024/3/13 17:05:49'
-		},
-		blitz: {
-			score: 389548,
-			play_at: '2024/1/7 16:24:20'
-		},
-		zen: {
-			score: 13383165,
-			level: 209
-		}
+		sprint: data.records['40l'].record ? {
+			time: (() => {
+				const time = data.records['40l'].record!.endcontext.finalTime / 1000
+
+				const hours = Math.trunc(time / 60)
+				const seconds = Math.trunc(time % 60)
+				const milliseconds = Math.trunc((time % 1) * 1000)
+
+				return `${hours}:${seconds}.${milliseconds}`
+			})(),
+			global_rank: data.records['40l'].rank,
+			play_at: data.records['40l'].record.ts
+		} : null,
+		blitz: data.records.blitz.record ? {
+			score: data.records.blitz.record.endcontext.score,
+			global_rank: data.records.blitz.rank,
+			play_at: data.records.blitz.record.ts
+		} : null,
+		zen: data.zen.score > 0 ? {
+			score: data.zen.score,
+			level: data.zen.level
+		} : null
 	})
 
 	document.querySelector('template#path')!.innerHTML = path
