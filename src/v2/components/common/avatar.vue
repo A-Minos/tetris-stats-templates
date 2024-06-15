4e05618f98096d3b16a3716acfa2a91d61fe65b6
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-06-15 23:45:57 +0800

    :sparkles: 失败时自动切换到 `identicon`

--- src/v2/components/common/avatar.vue
+++ src/v2/components/common/avatar.vue
@@ -1,35 +1,51 @@
 <script lang="ts" setup>
 import type Avatar from '@/shared/types/avatar'
+import { md5 } from 'hash-wasm'
 import Identicon from 'identicon.js'
 import { isString } from 'remeda'
 
 const props = defineProps<{
+	readonly user_id: string
 	readonly avatar: Avatar
 }>()
 
+const generateIdenticon = (hash: string) => {
+	// @ts-ignore
+	const result = new Identicon(hash, {
+		background: [8, 10, 6, 255],
+		margin: 0.15,
+		size: 300,
+		brightness: 0.48,
+		saturation: 0.65,
+		format: 'svg'
+	}).toString()
 
-const avatar_url = computed(() => {
-	if (isString(props.avatar)) {
-		return props.avatar
-	}
+	return `data:image/svg+xml;base64,${result}`
+}
 
-	switch (props.avatar.type) {
-		case 'identicon':
-			// @ts-ignore
-			const result = new Identicon(props.avatar.hash, {
-				background: [8, 10, 6, 255],
-				margin: 0.15,
-				size: 300,
-				brightness: 0.48,
-				saturation: 0.65,
-				format: 'svg'
-			}).toString()
+const url = ref<string>()
 
-			return `data:image/svg+xml;base64,${result}`
-	}
+const useIdenticon = async () => {
+	url.value = generateIdenticon(
+		await md5(props.user_id)
+	)
+}
+
+tryOnMounted(async () => {
+	url.value = await (async () => {
+		if (isString(props.avatar)) {
+			return props.avatar
+		}
+
+		switch (props.avatar.type) {
+			case 'identicon':
+				// @ts-ignore
+				return generateIdenticon(props.avatar.hash)
+		}
+	})()
 })
 </script>
 
 <template>
-	<n-avatar :src="avatar_url"/>
+	<n-avatar v-if="url" :src="url" @error="useIdenticon"/>
 </template>
